d20ca37654fcda4ea6d91bb0323c4a0adae82b34
Armando Santos <armando@well-typed.com>
2021-09-28 11:38:01 +0100

    Fixes async exception handling in Snocket.

--- ouroboros-network-framework/src/Simulation/Network/Snocket.hs
+++ ouroboros-network-framework/src/Simulation/Network/Snocket.hs
@@ -723,9 +723,11 @@ mkSnocket state tr = Snocket { getLocalAddr
                                    (STBearerInfo bearerInfo))
             -- connection delay
             unmask (threadDelay (biConnectionDelay bearerInfo `min` connectTimeout))
-              `catch` \(_ :: SomeException) ->
+              `catch` \(e :: SomeAsyncException) -> do
                 atomically $ modifyTVar (nsConnections state)
                                         (Map.delete (normaliseId connId))
+                throwIO e
+
             traceWith tr (WithAddr (Just (localAddress connId))
                                    (Just remoteAddress)
                                    (STDebug "delay:done"))
@@ -742,7 +744,7 @@ mkSnocket state tr = Snocket { getLocalAddr
               case (efd, lstFd) of
                 -- error cases
                 (_, Nothing) ->
-                  return (Left (connectIOError connId "no suh listening socket"))
+                  return (Left (connectIOError connId "no such listening socket"))
                 (_, Just FDUninitialised {}) ->
                   return (Left (connectIOError connId "unitialised listening socket"))
                 (_, Just FDConnecting {}) ->
@@ -792,7 +794,7 @@ mkSnocket state tr = Snocket { getLocalAddr
                 -- wait for a connection to be accepted
                 timeoutVar <-
                   registerDelay (connectTimeout - biConnectionDelay bearerInfo)
-                r <- unmask $ atomically $ runFirstToFinish $
+                r <- unmask (atomically $ runFirstToFinish $
                   (FirstToFinish $ do
                     LazySTM.readTVar timeoutVar >>= check
                     return Nothing
@@ -810,6 +812,10 @@ mkSnocket state tr = Snocket { getLocalAddr
                                          ++ show (normaliseId connId)
                       Just Connection { connState } ->
                         Just <$> check (connState == Established))
+                     )
+                  `onException`
+                     atomically (modifyTVar (nsConnections state)
+                                            (Map.delete (normaliseId connId)))
 
                 case r of
                   Nothing -> do
