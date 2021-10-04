ee05bfcbd1f6870e3f3535b714ed187b7cb959ae
Vuong hai Thanh <vuonghaithanh@MacBook-Pro-cua-Vuong.local>
2021-10-05 01:06:48 +0700

    fix buid

--- ouroboros-consensus-shelley/src/Ouroboros/Consensus/Shelley/Ledger/Ledger.hs
+++ ouroboros-consensus-shelley/src/Ouroboros/Consensus/Shelley/Ledger/Ledger.hs
@@ -93,6 +93,7 @@ import           Ouroboros.Consensus.Shelley.Ledger.TPraos ()
 import           Ouroboros.Consensus.Shelley.Protocol (MaxMajorProtVer (..),
                      Ticked (TickedPraosLedgerView))
 import           Ouroboros.Consensus.Shelley.Protocol.Util (isNewEpoch)
+import           Test.Cardano.Ledger.Shelley.Rules.Chain(CHAIN)
 
 {-------------------------------------------------------------------------------
   Ledger errors
